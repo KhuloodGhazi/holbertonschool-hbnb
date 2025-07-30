f3329c184849d9994ff3cd667e98b021022277d4
KhuloodGhazi <khulood.gmo@gmail.com>
2025-07-30 17:14:03 +0300

    Fix login

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -13,8 +13,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
     CORS(app,
-     resources={r"/api/*": {"origins": ["http://localhost:5500", "http://127.0.0.1:5500"]}},
-     supports_credentials=True)
+         resources={r"/api/*": {"origins": ["http://localhost:5500", "http://127.0.0.1:5500"]}})
 
     bcrypt.init_app(app)
     jwt.init_app(app)
